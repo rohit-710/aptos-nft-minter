8e9e69484d745b82e322293eee7ca1c2039f303b
Rohit Ramesh <rohitramesh@Rohits-MacBook-Pro.local>
2024-02-19 14:50:58 +0530

    replacing with Pusher fix-2

--- api/webhook.js
+++ api/webhook.js
@@ -23,9 +23,9 @@ export default async function handler(req, res) {
     res.status(405).end("Method Not Allowed");
   }
   console.log({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.PUSHER_KEY,
-    secret: process.env.PUSHER_SECRET,
-    cluster: process.env.PUSHER_CLUSTER,
+    appId: process.env.API_ID,
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    cluster: process.env.CLUSTER,
   });
 }
